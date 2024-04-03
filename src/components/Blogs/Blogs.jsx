ea2fab060e4c13b04bc04181aae9f1c1d0f97ae9
Almuheetu Shihab <almuheetu@gmail.com>
2024-04-04 01:44:23 +0600

    Blogs section fixed

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Blogs = () => {
+    const [blogs, setBlogs] =useState([]);
+    useEffect()
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
