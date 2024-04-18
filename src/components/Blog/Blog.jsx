11ac83386fb5166f8a6c8ec8ee690e88b9909930
Almuheetu Shihab <almuheetu@gmail.com>
2024-04-18 19:11:08 +0600

    proptypes passing

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,17 @@
+import PropTypes from 'prop-types';
 
-const Blog = () => {
+const Blog = ({blog}) => {
+    const {title} = blog;
     return (
         <div>
-            
+            <h2 className="text-4xl">{title}</h2>
         </div>
     );
 };
+
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
+}
+
 
 export default Blog;
