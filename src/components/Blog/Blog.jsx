f9de84a430046d98d3f16eb7a1fbe42952dadfb2
Almuheetu Shihab <almuheetu@gmail.com>
2024-04-18 22:03:20 +0600

    Cover img uploaded

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
-    const {title} = blog;
+    const {title,cover} = blog;
     return (
         <div>
+            <img src={cover} alt={'Cover picture of the title ${title}'} />
             <h2 className="text-4xl">{title}</h2>
         </div>
     );
