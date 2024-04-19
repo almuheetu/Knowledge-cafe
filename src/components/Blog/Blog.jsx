4ecbd15fbcbf6b0152f8e2376a1e7a757270ddd4
Almuheetu Shihab <almuheetu@gmail.com>
2024-04-19 17:08:01 +0600

    jeson format to data passing

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,19 +1,28 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
-    const {title,cover,author,author_img,reading_time} = blog;
+    const {title,cover,author,author_img,posted_date, reading_time,hashtags} = blog;
     return (
         <div>
             <img src={cover} alt={'Cover picture of the title ${title}'} />
             <div className='flex justify-between'>
-                <div>
-                    <img src={author_img} alt="" />
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
                 </div>
                 <div>
                     <span>{reading_time}min read</span>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map(hash => <span><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
