47cd1bf384533ffd441b2daa7f5c3721c469cf7b
Almuheetu Shihab <almuheetu@gmail.com>
2024-04-21 11:14:37 +0600

    bookmarks icon fix and react icon install

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import { FaBeer } from "react-icons/fa";
 
 const Blog = ({ blog }) => {
     const {title,cover,author,author_img,posted_date, reading_time,hashtags} = blog;
     return (
-        <div>
-            <img src={cover} alt={'Cover picture of the title ${title}'} />
-            <div className='flex justify-between'>
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={'Cover picture of the title ${title}'} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
                     <img className='w-14' src={author_img} alt="" />
                     <div className='ml-6'>
@@ -15,6 +16,7 @@ const Blog = ({ blog }) => {
                 </div>
                 <div>
                     <span>{reading_time}min read</span>
+                    <button><FaBeer></FaBeer></button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
