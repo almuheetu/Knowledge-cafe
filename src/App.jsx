68105ff37acad34d0401c84e5b84b8adf7f97549
Almuheetu Shihab <almuheetu@gmail.com>
2024-04-28 06:01:18 +0600

    bookmark button concacinate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
+  const[bookmarks, setBookmark] = useState([]);
+  
+
+  const handleAddToBookmark = blog =>{
+    console.log('bookmark adding soon')
+  }
 
 
   return (
@@ -11,7 +18,7 @@ function App() {
      
      <Header></Header>
      <div className='md:flex max-w-7xl mx-auto'>
-     <Blogs></Blogs> 
+     <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs> 
      <Bookmarks></Bookmarks>
      </div>
  
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { IoBookmarksOutline } from "react-icons/io5";
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, handleAddToBookmark }) => {
     const {title,cover,author,author_img,posted_date, reading_time,hashtags} = blog;
     return (
         <div className='mb-20'>
@@ -16,7 +16,7 @@ const Blog = ({ blog }) => {
                 </div>
                 <div>
                     <span>{reading_time}min read</span>
-                    <button className='ml-2 text-red-600'><IoBookmarksOutline /></button>
+                    <button onClick={handleAddToBookmark} className='ml-2 text-red-600'><IoBookmarksOutline /></button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
